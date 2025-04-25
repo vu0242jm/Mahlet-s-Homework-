6a346f7f0d4bba2e03567bb76bb110ff68ae3968
vu0242jm <vu0242jm@go.minnstate.edu>
2025-04-25 11:18:27 -0500

    Create ReverseString.cs

---
+++ ReverseString.cs
@@ -0,0 +1,21 @@
+// Written By Mahlet Gebremedin 
+// April 25/ 2025 
+
+public class Solution {
+    public void ReverseString(char[] s) {
+        int left = 0;
+        int right = s.Length - 1;
+
+        
+        while (left < right) {
+            
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+           
+            left++;
+            right--;
+        }
+    }
+}
